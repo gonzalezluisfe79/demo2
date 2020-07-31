73d7f1ed85c497d92aceaa6fa3671c8a76cc4ac5
gonzalezluisfe79 <luisfg79@gmail.com>
2020-07-30 22:52:26 -0400

    Primer R

---
+++ GITHUB.R
@@ -0,0 +1,8 @@
+library(datasets)
+data(iris)
+View(iris)
+unique(iris$Species)
+install.packages(c("GGally", "ggplot2"))
+unique(iris$Sepal.Width)
+library(GGally)
+ggpairs(iris, mapping=ggplot2::aes(colour = Species))
